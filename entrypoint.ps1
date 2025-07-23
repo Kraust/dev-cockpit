36693f0896d41f28770eabda7e9aab1b77e45b45
Kraust <secretdragoon@gmail.com>
2025-07-23 20:16:45 +0000

    More updates

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -1,13 +1,24 @@
-if (!Test-Parth $HOME/.cargo)
-{
-	curl https://sh.rustup.rs -sSf | sh -s -- -y
-	cargo install tree-sitter-cli
-	cargo install starship
-}
 
-if (!Test-Path /root/.config)
+if ($isWindows)
 {
-	cp -rf /opt/.config /root/
+	if (!Test-Path $HOME/AppData/Local/nvim)
+	{
+		cp -rf /opt/ $HOME
+	}
+	$ENV:PATH += ";C:\tools\neovim\nvim-win64\bin\"
+} else
+{
+	if (!Test-Parth $HOME/.cargo)
+	{
+		curl https://sh.rustup.rs -sSf | sh -s -- -y
+		cargo install tree-sitter-cli
+		cargo install starship
+	}
+
+	if (!Test-Path $HOME/.config)
+	{
+		cp -rf /opt/.config $HOME
+	}
 }
 
 nvim --headless --listen 0.0.0.0:9999
